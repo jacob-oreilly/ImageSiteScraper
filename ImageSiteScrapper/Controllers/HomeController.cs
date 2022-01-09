72f1f302a397e7bd9f39acdde14140b500397b17
jacob-oreilly <jakeor45@gmail.com>
2022-01-09 11:35:56 -0800

    Added intial single page scraper in search for all image tags

---
+++ ImageSiteScrapper/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ImageSiteScraper.Models;
+using System.Net.Http;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace ImageSiteScraper.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string url = "https://en.wikipedia.org/wiki/Main_Page";
+            var response = CallUrl(url).Result;
+            var allLinks = GetHrefTags(response);
+            var allImageUrls = GetImgTags(response);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private static async Task<string> CallUrl(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+            client.DefaultRequestHeaders.Accept.Clear();
+            var response = client.GetStringAsync(fullUrl);
+            return await response;
+        }
+        public IEnumerable<string> GetHrefTags(string htmlString)
+        {
+            HtmlDocument htmlSnippet = new HtmlDocument();
+            htmlSnippet.LoadHtml(htmlString);
+
+            List<string> hrefTags = new List<string>();
+
+            foreach (HtmlNode link in htmlSnippet.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                HtmlAttribute att = link.Attributes["href"];
+                hrefTags.Add(att.Value);
+            }
+
+            return hrefTags;
+        }
+
+        public IEnumerable<string> GetImgTags(string htmlString)
+        {
+            HtmlDocument htmlSnippet = new HtmlDocument();
+            htmlSnippet.LoadHtml(htmlString);
+
+            List<string> hrefTags = new List<string>();
+
+            foreach (HtmlNode link in htmlSnippet.DocumentNode.SelectNodes("//img[@src]"))
+            {
+                HtmlAttribute att = link.Attributes["src"];
+                hrefTags.Add(att.Value);
+            }
+
+            return hrefTags;
+        }
+    }
+}
